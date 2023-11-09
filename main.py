1808add0789823ae4a6385405f0edbb41e2d508e
kukhvatov <kukhvatov@mail.ru>
2023-11-09 19:43:54 +0500

    Нулевой этаж полностью готов RTF

--- main.py
+++ main.py
@@ -26,12 +26,12 @@ for building_name, building_data in data.items():
         add_rooms_and_connections(building_data, floor)
 
 
-start_room = "Room_01"
-end_room = "Room_21"
+start_room = "Stairs_0_2"
+end_room = "Room_50"
 shortest_path = nx.shortest_path(G, source=start_room, target=end_room)
 
 
-def draw_path_segment(draw, segment, color="red", width=5):
+def draw_path_segment(draw, segment, color="green", width=5):
     for i in range(len(segment) - 1):
         room_start = segment[i]
         room_end = segment[i + 1]
